235a9a977483189806607a5b5e14cd2e14c29ac2
Paul Done <pkdone@gmail.com>
2022-01-04 21:01:49 +0000

    Typos

--- examples/pipeline_example_fake_accounts.js
+++ examples/pipeline_example_fake_accounts.js
@@ -5,11 +5,11 @@ pipeline = [
         "card_number": fakePaddedNumberAsText(16),                                             // 16 digit textual credit card number
         "card_start": fakeDateBeforeNow(2*365*24*60*60*1000),                                  // Up to 2 years before now
         "card_expiry": fakeDateAfterNow(365*24*60*60*1000),                                    // Up to 1 year after now
-        "rank": fakeNumber(10),                                                                // Any number up to 15 digits in size
+        "rank": fakeNumber(10),                                                                // Any number up to 10 digits in size
         "reported": fakeBoolean(),                                                             // True of False - 50:50
         "flagged": fakeBooleanWeighted(15),                                                    // 15% time True, rest of time False
         "interestRate": fakeDecimal(),                                                         // Random decimal number between 0.0 and 1.0
-        "balance": fakeDecimalSignificantPlaces(8),                                            // Random decimal number to significant places
+        "balance": fakeDecimalSignificantPlaces(8),                                            // Random decimal number to 8 significant places
         "cardType": fakeValueFromList(["CREDIT", "DEBIT", "VOUCHER"]),                         // Randomly set to one of CREDIT | DEBIT | VOUCHER
         "classification": fakeValueFromListWeighted(["A+", "B", "C", "D"]),                    // Random value more likely from later part of list
         "address.number": fakeNumberBounded(1, 99),                                            // Random house number between 1-99
@@ -17,6 +17,6 @@ pipeline = [
         "address.town": fakeTownName(),                                                        // Random town name         
         "address.country": fakeCountryName(),                                                  // Random country name
         "address.zipcode": fakeZipCode(),                                                      // Random zip code
-        "records": fakeListOfSubDocs(4, ["BLUE", "RED", "GREEN", "PURPLE", "ORANGE", "PINK"]), // Array of 5 random subdocuments
+        "records": fakeListOfSubDocs(4, ["BLUE", "RED", "GREEN", "PURPLE", "ORANGE", "PINK"]), // Array of 4 random subdocuments
     }},
 ]
